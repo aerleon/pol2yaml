490264c9c3432a898df7c7970f2500c153989dba
Jason Benterou <jason.benterou@gmail.com>
2023-05-11 22:19:39 +0000

    Test example files in test matrix

--- tests/pol2yaml.test.js
+++ tests/pol2yaml.test.js
@@ -1,5 +1,6 @@
+import * as fs from "node:fs/promises";
+import { join } from 'node:path';
 import {describe, test} from "node:test";
-import assert from "node:assert";
 
 import assertSnapshot from "snapshot-assertion";
 
@@ -15,26 +16,25 @@ term deny-all {
     action:: deny
 }`;
 
-const TEST_SIMPLE_YAML = `filters:
-  - header:
-      target:
-        srx: from-zone A to-zone B
-    terms:
-      - name: deny-all
-        action: deny
-`;
-
 const TEST_SIMPLE_INC = `
 term deny-all {
     action:: deny
 }`;
 
-describe("Convert .pol files", () => {
+test("Convert .pol files", async t => {
     const TEST_MATRIX = [
         {name:"Simple policy", key: "simple_policy", pol: TEST_SIMPLE_POL}
     ];
+    const pol_dir = './policies/pol/';
+    const example_files = await fs.readdir(pol_dir);
+    for (const file_name of example_files) {
+        if (/.pol$/.test(file_name)) {
+            const contents = await fs.readFile(join(pol_dir, file_name));
+            TEST_MATRIX.push({name: `Example file ${file_name}`, key: file_name, pol: String(contents)});
+        }
+    }
     for (const {name, key, pol} of TEST_MATRIX) {
-        test(name, async t => {
+        await t.test(name, async t => {
             const output = new PolicyFile('', pol, false).toYAML();
             await assertSnapshot(output, `tests/${key}.yaml.ref`);
         });
